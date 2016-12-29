4dfa60459383cdf4a4fc6e0121e376d2a9418196
Yutae Choi <93yutaechoi@gmail.com>
2016-12-29 17:42:58 +0900

    Wiring up Buttons (IBOutlets+IBActions)

---
+++ PlaySoundsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by 최유태 on 2016. 12. 29..
+//  Copyright © 2016년 YutaeChoi. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    var recordedAudioURL: URL!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
